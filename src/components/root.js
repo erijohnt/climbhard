f2ca18b145145bcc5e7aad79e19fc0098a6334c9
Eli Tran-Johnson <erijohnt@cs.uchicago.edu>
2019-06-06 15:05:31 -0500

    semantic ui

--- src/components/root.js
+++ src/components/root.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import {csv} from 'd3-fetch';
-import ExampleChart from './example-chart';
-import SimpleMarkers from './map';
+import React from "react";
+import { csv } from "d3-fetch";
+import ExampleChart from "./example-chart";
+import SimpleMarkers from "./map";
+import {
+  Container,
+  Header,
+  Divider,
+  Placeholder,
+  Segment
+} from "semantic-ui-react";
 
 const longBlock = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -10,6 +17,21 @@ laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repreh
 voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
 non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 `;
+
+const PlaceholderExamplePlaceholder = () => (
+  <Placeholder fluid>
+    <Placeholder.Header>
+      <Placeholder.Line />
+      <Placeholder.Line />
+    </Placeholder.Header>
+    <Placeholder.Paragraph>
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+    </Placeholder.Paragraph>
+  </Placeholder>
+);
 
 class RootComponent extends React.Component {
   constructor() {
@@ -21,29 +43,36 @@ class RootComponent extends React.Component {
   }
 
   componentWillMount() {
-    csv('data/sample-data.csv')
-      .then(data => {
-        this.setState({
-          data,
-          loading: false
-        });
+    csv("data/sample-data.csv").then(data => {
+      this.setState({
+        data,
+        loading: false
       });
+    });
   }
 
   render() {
-    const {loading, data} = this.state;
+    const { loading, data } = this.state;
     if (loading) {
       return <h1>LOADING</h1>;
     }
     return (
-      <div className="relative">
-        <h1> Hard Climbs Map </h1>
-        <div>{longBlock}</div>
-        <SimpleMarkers />
-        <div>{longBlock}</div>
-      </div>
+      <Container>
+        <Segment basic />
+        <Header dividing size="huge" className="title">
+          Climb Hard, Bro
+        </Header>
+        <Container text>
+          <PlaceholderExamplePlaceholder />
+          <Header>Hard Climbs</Header>
+          <Segment>
+            <SimpleMarkers />
+          </Segment>
+          <PlaceholderExamplePlaceholder />
+        </Container>
+      </Container>
     );
   }
 }
-RootComponent.displayName = 'RootComponent';
+RootComponent.displayName = "RootComponent";
 export default RootComponent;
